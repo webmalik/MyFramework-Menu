2a547de58096015ce8d0958c568e3211ed68915b
webmalik <program.malik@gmail.com>
2016-02-24 11:05:02 +0200

    MyFramework | Menu v1.0.0

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	function windowSize(){
+	    if ($(window).width() <= '750'){
+	        $('#toggle-menu').show();
+	        $('ul.nav-list').css("display", "none");
+	    } else {
+	        $('#toggle-menu').hide();
+	        $('ul.nav-list').css("display", "table-row");
+	    }
+	}
+
+	$(window).on('load resize',windowSize);
+
+	var i=0;
+
+	$("#toggle-menu").click(function(){
+		if(i == 0) {
+			$("ul.nav-list").animate({ height: "show" }, 500);
+			i = 1;
+		}else {
+			$("ul.nav-list").animate({ height: "hide" }, 500);
+			i = 0;
+		}
+		console.log(i);
+	});
+});
